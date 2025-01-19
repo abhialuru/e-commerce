87d91b10472286de5f9733aa5f99b6188237688c
abhialuru <alurabhi9346@gmail.com>
2025-01-19 22:58:44 +0530

    errors resolved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,9 @@ const eslintConfig = [
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/rules-of-hooks": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-vars": "off"       
     }
   }
 ];
